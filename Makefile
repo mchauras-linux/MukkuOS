2df3ce300b4c90a687e376d0fc175e30c36140c4
Mukesh Kumar Chaurasiya <mukeshmike9@gmail.com>
2021-10-30 02:54:42 +0530

    GDT Descriptor Broken

---
+++ Makefile
@@ -0,0 +1,10 @@
+all:
+	nasm -f bin boot.asm -o boot.bin
+	dd if=message.txt >> boot.bin
+	dd if=/dev/zero bs=512 count=1 >> boot.bin
+
+run: all
+	qemu-system-x86_64 -hda boot.bin
+
+clean:
+	rm -rf boot.bin
--- boot.asm
+++ boot.asm
@@ -1,5 +1,9 @@
 ORG 0
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 _start:
     jmp short start
     nop
@@ -8,38 +12,19 @@ times 33 db 0
 start:
     jmp 0x7c0:step2
 
-handle_zero:
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0x00
-    int 0x10
-    iret
+read_hdd:
+    mov ah, 2       ; Read Sector command
+    mov al, 2       ; Two Sector to read
+    mov ch, 0       ; Cylinder low eight bits
+    mov cl, 2       ; Read Sector 2
+    mov dh, 0       ; Head Number
+    mov bx, buffer
+    int 0x13
+    jc error
+    ret
 
-handle_one:
-    mov ah, 0eh
-    mov al, 'V'
-    mov bx, 0x00
-    int 0x10
-    iret
-
-step2:
-    cli ; Clear Interrupts
-    mov ax, 0x7c0
-    mov ds, ax
-    mov es, ax
-    mov ax, 0x00
-    mov ss, ax
-    mov sp, 0x7c00
-    sti ; Enables Interrupts
-
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7C0
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7C0
-
-    int 1
-
-    mov si, message
+error:
+    mov si, error_message
     call print
     jmp $
 
@@ -56,11 +41,87 @@ print:
     ret
 ;Print Character
 print_char:
+    cmp al, 0x0A
+    je print_new_line
     mov ah, 0eh
     int 0x10
     ret
 
-message: db 'Booting Mukku OS...', 0
+print_new_line:
+    mov al, 0x0A
+    mov ah, 0eh
+    int 0x10
+    mov al, 0x0D        ; Carriage Return
+    int 0x10
+    ret
+
+step2:
+    cli             ; Clear Interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti             ; Enables Interrupts
+
+    call read_hdd   ; Read Booting Message
+    mov si, buffer
+    call print
+
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+error_message: db 'Failed to read HDD', 0
+
+;GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+
+;[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+    jmp $
+
 times 510 - ($ - $$) db 0
 db 0x55
 db 0xAA
+
+buffer:
